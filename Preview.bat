6e92db42ccc0500333d716858a452bda51ea42d1
Tianya <914602509@qq.com>
2022-04-07 20:42:31 +0800

    自动拉取图片目录；

--- Preview.bat
+++ Preview.bat
@@ -1,3 +1,11 @@
+@REM clone并更新图片目录
+if not exist "static/images" (
+    git clone git@github.com:Chz-yang/GitHubPageImages.git static/images
+)
+cd static/images
+git pull .
+cd ../../
+
 @REM 创建图片目录的软链
 if not exist "images" (
     mklink /D .\images .\static\images
